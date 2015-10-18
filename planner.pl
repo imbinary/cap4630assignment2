ac348a2dc6fa14f5fe7a9047331df03b707acbf6
Chris Orem <chrisorem@gmail.com>
2015-10-18 19:53:34 -0400

    add working 2room

--- planner.pl
+++ planner.pl
@@ -24,6 +24,14 @@ plan(State, Goal, _, Moves) :-	equal_set(State, Goal),
 				write('moves are'), nl,
 				reverse_print_stack(Moves).
 plan(State, Goal, Been_list, Moves) :-
+				move2(Name, Preconditions, Actions),
+				conditions_met(Preconditions, State),
+				change_state(State, Actions, Child_state),
+				not(member_state(Child_state, Been_list)),
+				stack(Child_state, Been_list, New_been_list),
+				stack(Name, Moves, New_moves),
+			plan(Child_state, Goal, New_been_list, New_moves),!.
+plan(State, Goal, Been_list, Moves) :-
 				move(Name, Preconditions, Actions),
 				conditions_met(Preconditions, State),
 				change_state(State, Actions, Child_state),
@@ -44,32 +52,51 @@ member_state(S, [_|T]) :-	member_state(S, T).
 
 /* move types */
 
-move(pickup(X), [handempty, clear(X), on(X, Y), inroom(X,Z), inroom(hand,Z)],
+
+move(pickup(X), [handempty, clear(X), on(X, Y)],
 		[del(handempty), del(clear(X)), del(on(X, Y)),
 				 add(clear(Y)),	add(holding(X))]).
 
-move(pickup(X), [handempty, clear(X), ontable(X), inroom(X,Z), inroom(hand,Z)],
+move(pickup(X), [handempty, clear(X), ontable(X)],
 		[del(handempty), del(clear(X)), del(ontable(X)),
 				 add(holding(X))]).
+
+move(stack(X, Y), [holding(X), clear(Y)],
+		[del(holding(X)), del(clear(Y)), add(handempty), add(on(X, Y)),
+				  add(clear(X))]).
 
 move(putdown(X), [holding(X)],
 		[del(holding(X)), add(ontable(X)), add(clear(X)),
 				  add(handempty)]).
 
-move(stack(X, Y), [holding(X), clear(Y), inroom(Y,Z), inroom(hand,Z)],
+
+
+move2(pickup(X), [handempty, clear(X), on(X, Y), inroom(X,Z), inroom(hand,Z)],
+		[del(handempty), del(clear(X)), del(on(X, Y)),
+				 add(clear(Y)),	add(holding(X))]).
+
+move2(pickup(X), [handempty, clear(X), ontable(X), inroom(X,Z), inroom(hand,Z)],
+		[del(handempty), del(clear(X)), del(ontable(X)),
+				 add(holding(X))]).
+
+move2(stack(X, Y), [holding(X), clear(Y), inroom(Y,Z), inroom(hand,Z)],
 		[del(holding(X)), del(clear(Y)), add(handempty), add(on(X, Y)),
 				  add(clear(X))]).
 
-move(goroom2, [holding(X), inroom(hand,1)],
+move2(putdown(X), [holding(X)],
+		[del(holding(X)), add(ontable(X)), add(clear(X)),
+				  add(handempty)]).
+
+move2(goroom2, [holding(X), inroom(hand,1)],
 		[del(inroom(X,1)), add(inroom(X,2)), del(inroom(hand,1)), add(inroom(hand,2))]).
 
-move(goroom1, [holding(X), inroom(hand,2)],
+move2(goroom1, [holding(X), inroom(hand,2)],
 		[del(inroom(X,2)), add(inroom(X,1)), del(inroom(hand,2)), add(inroom(hand,1))]).
 
-move(goroom2, [handempty, inroom(hand,1)],
+move2(goroom2, [handempty, inroom(hand,1)],
 		[del(inroom(hand,1)), add(inroom(hand,2))]).
 
-move(goroom1, [handempty, inroom(hand,2)],
+move2(goroom1, [handempty, inroom(hand,2)],
 		[del(inroom(hand,2)), add(inroom(hand,1))]).
 
 /* helper stuff */
